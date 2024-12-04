94efa5287f66179e9d65be2f205bbac1b59e55de
Amirzhan Albariyev <albariev123@gmail.com>
2024-12-04 19:18:46 +0300

    Add try finally

--- send_comics.py
+++ send_comics.py
@@ -3,7 +3,6 @@ from environs import Env
 from comics_functions import delete_picture, get_random_comics_num, download_picture, get_comics
 
 def main():
-
     env = Env()
     env.read_env()
 
@@ -11,20 +10,21 @@ def main():
     bot = telegram.Bot(token=env.str('TG_BOT_TOKEN'))
     chat_id = env.str('TG_CHAT_ID')
 
-    random_comics_url = get_random_comics_num()
+    try:
+        random_comics_url = get_random_comics_num()
 
-    comics = get_comics(random_comics_url)
-    download_picture(filename, comics['img'])
-    comics_comment = comics['alt']
-    
-    with open(filename, 'rb') as image:
-        bot.send_document(
-            chat_id=chat_id,
-            document=image,
-            caption=comics_comment
-        )
+        comics = get_comics(random_comics_url)
+        download_picture(filename, comics['img'])
+        comics_comment = comics['alt']
         
-    delete_picture(filename)
+        with open(filename, 'rb') as image:
+            bot.send_document(
+                chat_id=chat_id,
+                document=image,
+                caption=comics_comment
+            )
+    finally:
+        delete_picture(filename)
 
 if __name__ == '__main__':
     main()
